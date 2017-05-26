7d30e3a151e287928b22c4bb6dfb69be77913f47
rockandre <rockandre31@gmail.com>
2017-05-26 15:15:40 +0100

    Commit to merge all

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Request as Requests;
 use App\Printer;
-use Illuminate\Support\Facades\Auth;
 
 class RequestController extends Controller
 {
